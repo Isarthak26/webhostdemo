9a90c2f67fd9d28c3e24d0fa66a5283a3328b5bd
sarthak bordia <151200027+Isarthak26@users.noreply.github.com>
2025-01-19 18:26:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+console.log("Hello World");
+// function solve(){
+//     var age  = 19;
+//     console.log(age);
+// }
+// solve();
+
+// var x = 10; 
+// var x = 20;
+
+// {
+// let a = 10;
+// }
+// console.log(a);
+
+
+// let a = 20;
+// a = "Sarthak";
+// a = null;
+// a = true;
+
+// const a = 20;
+// console.log(a);
+
+// const a = 10;
+// console.log(a);
